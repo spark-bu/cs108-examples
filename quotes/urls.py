649a4eba9dd1665f17a467f2ae59064b297ef575
Soowhan Park <spark96@bu.edu>
2020-03-31 01:18:55 +0900

    ex16

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, QuotePageView, RandomQuotePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name = 'home'),
+    path('', RandomQuotePageView.as_view(), name='random'),
+    path('all', HomePageView.as_view(), name='home'),
+    path('quote/<int:pk>', QuotePageView.as_view(), name='home'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Quote
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+import random
 
 class HomePageView(ListView):
     model = Quote
     template_name = 'quotes/home.html'
     context_object_name = 'all_quotes_list'
+
+class QuotePageView(DetailView):
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+class RandomQuotePageView(DetailView):
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+    def get_object(self):
+        all_quotes = Quote.objects.all()
+        r = random.randint(0, len(all_quotes))
+        q = all_quotes[r]
+        return q 
